f11f9c57c92fc4cc0f1422470e739b61bd5aa5f2
Chris Jones <cjones1688@gmail.com>
2025-09-22 16:54:50 -0400

    initial setup

--- plots.R
+++ plots.R
@@ -4,5 +4,13 @@ install.packages("ggplot2")
 library(terra)
 library(ggplot2)
 
+data_path <- "Z:/Data/PoPS Runs/Spotted Lanternfly/spotted_latternfly/whole_usa/5km"
+map_data <- rast(file.path(data_path, "probability_30_year.tif"))
 
-map_data <- rast()
+
+plot(map_data[[1]])
+
+
+df_data <- file.path(data_path, "crop_risk2.csv")
+
+ggplot(df_data)
